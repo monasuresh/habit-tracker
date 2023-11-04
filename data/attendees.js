b4d3a7f1dfbec86f7196206ad91bc47299e4429d
monasuresh <monicasuresh45@gmail.com>
2023-11-04 18:59:11 -0400

    Creating Initial Project Structure #22

---
+++ data/attendees.js
@@ -0,0 +1,17 @@
+// This data file should export all functions using the ES6 standard as shown in the lecture code
+
+const createAttendee = async (eventId, firstName, lastName, emailAddress) => {
+  //Implement Code here
+};
+
+const getAllAttendees = async (eventId) => {
+  //Implement Code here
+};
+
+const getAttendee = async (attendeeId) => {
+  //Implement Code here
+};
+
+const removeAttendee = async (attendeeId) => {
+  //Implement Code here
+};
---
+++ data/events.js
@@ -0,0 +1,45 @@
+// This data file should export all functions using the ES6 standard as shown in the lecture code
+
+const create = async (
+  eventName,
+  eventDescription,
+  eventLocation,
+  contactEmail,
+  maxCapacity,
+  priceOfAdmission,
+  eventDate,
+  startTime,
+  endTime,
+  publicEvent
+) => {
+  //Implement Code here
+  //Do NOT forget to initalize attendees to be an empty array and totalNumberOfAttendees to 0 on event creation
+};
+
+const getAll = async () => {
+  //Implement Code here
+};
+
+const get = async (eventId) => {
+  //Implement Code here
+};
+
+const remove = async (eventId) => {
+  //Implement Code here
+};
+
+const update = async (
+  eventId,
+  eventName,
+  eventDescription,
+  eventLocation,
+  contactEmail,
+  maxCapacity,
+  priceOfAdmission,
+  eventDate,
+  startTime,
+  endTime,
+  publicEvent
+) => {
+  //Implement Code here
+};
